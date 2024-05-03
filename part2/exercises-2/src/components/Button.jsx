1da856a866ca1372543fbefa0ebcdc7ace6460cc
sunsonni <sonniemnguyen@gmail.com>
2024-05-03 08:24:38 -0500

    react 2 exercises

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -1,9 +1,12 @@
 import './styles.css';
 
 function Button() {
+   function onLearnMore(){
+      alert("Splishie Splashie");
+   }
 
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
